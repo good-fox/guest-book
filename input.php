2d45000f256ed59c602acf6ea03462a584a936e2
Vadym <skoroplias.official@gmail.com>
2019-09-29 16:45:24 +0300

    Update input.php

---
+++ input.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Гостюва книга</title>
+  </head>
+  <body>
+    <?php require_one('views/header.php') ?>
+    <div class="blog">
+      <h1>Text blog.)</h1>
+    </div>
+    <?php require_one('views/footer.php') ?>
+  </body>
+</html>
