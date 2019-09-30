922c1c7f72579ac4f925c3777d212682b31aa954
good-fox <55841620+good-fox@users.noreply.github.com>
2019-09-30 16:01:18 +0300

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,7 +1,7 @@
-<h1>
-  <b>
-    <em>
-      <a href="../guest-book/index.php">Главная страница</a>
-    </em>
-  </b>
-</h1>
+<h2>
+  <em>
+    <a href="../guest-book/index.php">Главная страница</a>
+    ________________
+    <a href="../guest-book/index.php?action=add">(Оставить отзыв)</a>
+  </em>
+</h2>
