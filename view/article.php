69e01fb4ce6677a32350c6c406c6a9e0b1cd10ec
good-fox <55841620+good-fox@users.noreply.github.com>
2019-09-30 14:43:15 +0300

    Create article.php

---
+++ view/article.php
@@ -0,0 +1,7 @@
+<div class="article">
+  <h1><?=$article['title']?></h1>
+    <em>Опубліковано: </em>
+    <strong><?=$article['name']?> </strong>
+    <em>- (<?=$article['email']?>) : <?=$article['date']?>.</em>
+    <p><?=$article['content']?></p>
+</div>
