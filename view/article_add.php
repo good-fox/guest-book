f60ba022107f59893716c2e889702546ac673bcd
good-fox <55841620+good-fox@users.noreply.github.com>
2019-09-30 16:36:17 +0300

    Update article_add.php

--- view/article_add.php
+++ view/article_add.php
@@ -15,32 +15,26 @@ if (!empty($_POST)) {
 
 <div>
   <form action="index.php?action=<?=$_GET['action']?><?php if (!empty($_GET['id'])) echo "&id=" . $_GET['id'];?>" method="post">
-
     <label>
       Имя
       <input type="text" name="name" value="<?php if (!empty($article['name'])) echo $article['name'];?>" class="form-item" autofocus required>
     </label>
-
     <label>
       e-mail
       <input type="email" name="email" value="<?php if (!empty($article['email'])) echo $article['email'];?>" class="form-item" autofocus required>
     </label>
-
     <label>
       Назва
       <input type="text" name="title" value="<?php if (!empty($article['title'])) echo $article['title'];?>" class="form-item" autofocus required>
     </label>
-
     <label>
       Дата
       <input type="date" name="date" value="<?php if (!empty($article['date'])) echo $article['date'];?>" class="form-item" required>
     </label>
-
     <label>
       Текст
       <textarea name="content" class="form-item" required><?php if (!empty($article['content'])) echo $article['content'];?></textarea>
     </label>
-
     <input type="submit" value="Зберигти" class="btn">
   </form>
 </div>
