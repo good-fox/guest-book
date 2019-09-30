2f4d18aa20aa1803ff8acf4c235d67c197050908
good-fox <55841620+good-fox@users.noreply.github.com>
2019-09-30 17:10:05 +0300

    Update fundb.php

--- metods/fundb.php
+++ metods/fundb.php
@@ -57,6 +57,21 @@ class SQL
     return true;
   }
 
+  static public function comment_get($db, $id) {
+    $query = "SELECT * FROM `comment` WHERE id=$id";
+    $result = mysqli_query($db, $query);
+
+    $n = mysqli_num_rows($result);
+    $comment = array();
+
+    for ($i=0; $i < $n; $i++) {
+      $row = mysqli_fetch_assoc($result);
+      $comment[] = $row;
+    }
+
+    return $comment;
+  }
+
 }
 
 ?>
