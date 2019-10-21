4e808e1cc925387e65843bc35fae8eb965717862
Vadym <skoroplias.official@gmail.com>
2019-10-21 21:13:17 +0300

    Update head.php

--- head.php
+++ head.php
@@ -1,4 +1,6 @@
 <head>
   <meta charset="utf-8">
   <title>Гостюва книга</title>
+  <link rel="stylesheet" href="../guest-book/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
