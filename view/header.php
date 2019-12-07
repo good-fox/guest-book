705674d526145050c58db199646fe6555215eef5
Vadym <skoroplias.official@gmail.com>
2019-12-07 16:24:44 +0200

    Update header.php

--- view/header.php
+++ view/header.php
@@ -3,5 +3,7 @@
     <a href="../guest-book/index.php">Главная страница</a>
     ________________
     <a href="../guest-book/index.php?action=add">(Оставить отзыв)</a>
+    ________________
+    <a href="../guest-book/index.php?action=admin">Вход</a>
   </em>
 </h2>
