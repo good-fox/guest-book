94eaebae78f277bfe8ff72ca5ba073f5da5a73c1
Vadym <skoroplias.official@gmail.com>
2019-12-08 02:01:56 +0200

    Update header.php

--- view/header.php
+++ view/header.php
@@ -4,6 +4,12 @@
     ________________
     <a href="../guest-book/index.php?action=add">(Оставить отзыв)</a>
     ________________
-    <a href="../guest-book/index.php?action=admin">Вход</a>
+    <?php if (!$_SESSION['admin']): ?>
+      <a href="../guest-book/index.php?action=admin">Вход</a>
+    <?php endif; ?>
+
+    <?php if ($_SESSION['admin']): ?>
+      <a href="../guest-book/index.php?action=exit">Вихід</a>
+    <?php endif; ?>
   </em>
 </h2>
