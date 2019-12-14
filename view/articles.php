777add200b3cfeac017b2645293f9c3cb4bcb2ef
Vadym <skoroplias.official@gmail.com>
2019-12-14 23:49:49 +0200

    Update articles.php

--- view/articles.php
+++ view/articles.php
@@ -1,7 +1,7 @@
 <div>
   <?php foreach ($article as $a): ?>
   <div class="article">
-    <h2><a href="?id=<?=$a['id']?>"><?=$a['title']?></a></h2>
+    <h4><a href="?id=<?=$a['id']?>"><?=$a['title']?></a></h4>
     <em>Опубліковано: </em>
     <strong><?=$a['name']?> </strong>
     <em>- (<?=$a['email']?>) : <?=$a['date']?>.</em>
@@ -10,7 +10,7 @@
     if (!empty($_SESSION['admin'])) {
       if ($_SESSION['admin']) {
         $id_temp = $a['id'];
-        echo "<a href='../guest-book/index.php?id=$id_temp&action=delete'>Удалить отзив</a>"; 
+        echo "<a href='../guest-book/index.php?id=$id_temp&action=delete'>Удалить отзив</a>";
       }
     } ?></p>
   </div>
